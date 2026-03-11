5faaf8ebdd793ae93a70fed7ae0d5cf8231a70e3
Raviraghul S <ravi.siva1064@gmail.com>
2026-03-11 16:55:55 +0530

    Update Test.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,20 @@
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(8, app.add(5,3));
+    }
+
+    @Test
+    public void testSubtract() {
+        App app = new App();
+        assertEquals(2, app.subtract(5,3));
+    }
+
+}
